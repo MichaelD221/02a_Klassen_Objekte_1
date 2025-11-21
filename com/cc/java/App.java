a25730d65649c55ba2cafc8138f557aadc489aee
Michael <atlasbook@web.de>
2025-11-21 11:34:06 +0100

    Cat abschluss

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,13 +4,31 @@ public class App {
     
     public static void main(String[] args) {
 
-        // new Cat();
-        System.out.println(new Cat());
-        
+        // System.out.println(new Cat());
 
+        // Instanziierung
+        // cat = Referenzvariable
+        // Cat = Referenztyp
+
+        Cat cat = new Cat();
+        output("Blick von aussen: " + cat);
+        output("Blick von innen: " + cat.getInstanceVariable());   
+        // cat.tellYourAddress();
+        output("-----------------------");        
+
+        Cat cat2 = new Cat();
+        output("Blick von aussen: " + cat2);
+        output("Blick von innen: " + cat2.getInstanceVariable());   
+        output("-----------------------");        
     }
 
-   
+
+
+    //statische Methode
+    public static void output(String outputStr){
+        System.out.println(outputStr);
+    }
+
 
 }
 
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -3,6 +3,15 @@ package com.cc.java;
 public class Cat {
 
 
+    // this --> Instanzvariable
+    public void tellYourAddress(){
+        App.output("Blick von innen: " + this);
+    }
+
+    public Cat getInstanceVariable(){
+        return this;
+    }
 
     
+
 }
